718c7fea935d10576546fcc49125c92029cc2a8d
Balaji V <balajivairavan@gmail.com>
2024-04-03 22:28:26 +0530

    second commit

---
+++ Demo1.java
@@ -0,0 +1,34 @@
+package ui;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		/*int a;
+		int b;
+		int c;
+		
+		a=100;
+		b=800;
+		//c=a+b;
+		System.out.println("sum of value: " + (a+b));*/
+		
+		int a=100;
+		double b=105.66;
+		char c='y';
+		String d="Balaji";
+		boolean e=false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}   
